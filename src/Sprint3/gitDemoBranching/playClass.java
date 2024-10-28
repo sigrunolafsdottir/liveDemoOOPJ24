4139a37c7922c551cc4a608843307e548e442b9b
sigrunolafsdottir <solafsdottir@gmail.com>
2024-10-28 11:00:04 +0100

    playbranch demo

--- src/Sprint3/gitDemoBranching/playClass.java
+++ src/Sprint3/gitDemoBranching/playClass.java
@@ -5,5 +5,6 @@ public class playClass {
     public static void main(String[] args) {
         System.out.println("testar att brancha");
         System.out.println("testar att brancha ännu mer");
+        System.out.println("doing stuff in playBranch");
     }
 }
